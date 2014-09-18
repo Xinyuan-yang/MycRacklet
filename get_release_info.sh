205223763cc914babbeff1a133c3455d9fd4d261
Fabian Barras <fabian.barras@epfl.ch>
2014-09-18 11:58:23 +0200

    git release info

--- get_release_info.sh
+++ get_release_info.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-svn diff > $1/release.patch
-svn --verbose status > $1/full-revision-detail.txt
+git diff > $1/release.patch
+git show > $1/full-revision-detail.txt
 echo "#include <string>" 
 echo "std::string lm_release_info =  \" \\n \\" 
-svn info  | sed s/\"/\\\\\"/g | sed s/\\\*/\\\\\*/g | sed 's/$/ \\n\\/'
-#svn diff  |  sed "s/\\\\/\\\\\\\\/" | sed s/\"/\\\\\"/g  | sed 's/$/ \\n \\/' | sed "s/\\\\\*/\\\\\\\\*/g" | sed "s/\\\\\//\\\\\\\\\//g" 
+git log -1 --pretty=fuller  | sed s/\"/\\\\\"/g | sed s/\\\*/\\\\\*/g | sed 's/$/ \\n\\/'
+git status -uno  |  sed "s/\\\\/\\\\\\\\/" | sed s/\"/\\\\\"/g  | sed 's/$/ \\n \\/' | sed "s/\\\\\*/\\\\\\\\*/g" | sed "s/\\\\\//\\\\\\\\\//g" 
 echo "\";"

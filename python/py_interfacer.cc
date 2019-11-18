6eba647953b6f8bad63ff074f116f5316c041490
Thibault Roch <thibault.roch@epfl.ch>
2019-11-18 18:18:20 +0100

    proper templated classes

--- python/py_interfacer.cc
+++ python/py_interfacer.cc
@@ -1,4 +1,4 @@
-#include <interfacer.cc>
+#include <interfacer.hh>
 /* -------------------------------------------------------------------------- */
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -9,18 +9,58 @@ namespace py = pybind11;
 /* -------------------------------------------------------------------------- */
 namespace cRacklet {
 /* -------------------------------------------------------------------------- */
+std::string FractureLawToString(FractureLawType F) {
 
+  std::stringstream str;  
+  std::string name;
+  
+  switch(F)
+    {
+    case _linear_coupled_cohesive:
+      name = "LinearCoupledCohesive"; 
+      break;
+      
+    case _rate_and_state:
+      name = "RateAndState"; 
+      break;
+
+    case _regularized_rate_and_state:
+      name = "RegularizedRateAndState"; 
+      break;
+
+    case _weakening_rate_and_state:
+      name = "WeakeningRateAndState";  
+      break;
+    }
+  
+  str << "Interfacer" << name;  
+  return str.str();
+}
+  
 void register_fracture_law_type(py::module&mod){
   py::enum_<FractureLawType>(mod,"FractureLawType")
-    .value("_linear_coupled_cohesive",FractureLawType::_linear_coupled_cohesive);
+    .value("_linear_coupled_cohesive",FractureLawType::_linear_coupled_cohesive)
+    .value("_rate_and_state",FractureLawType::_rate_and_state)
+    .value("_regularized_rate_and_state",FractureLawType::_regularized_rate_and_state)
+    .value("_weakening_rate_and_state",FractureLawType::_weakening_rate_and_state);
   }
   
-//template<FractureLawType F>
-void register_interfacer(py::module& mod) {
-  FractureLawType const F = _linear_coupled_cohesive;
-  py::class_<Interfacer<F>,DataRegister>(mod, "Interfacer")
+template<FractureLawType F>
+void wrap_interfacer_RANDS(py::module& mod) {
+  std::string class_name = FractureLawToString(F);
+  py::class_<Interfacer<F>,DataRegister>(mod,class_name.c_str())
+    .def(py::init<SpectralModel&>())
+    .def("createUniformInterface",&Interfacer<F>::createUniformInterface);
+}
+
+  
+template<FractureLawType F>
+void wrap_interfacer_cohesive(py::module& mod) {
+  std::string class_name = FractureLawToString(F);
+  py::class_<Interfacer<F>,DataRegister>(mod,class_name.c_str())
     .def(py::init<SpectralModel&>())
     .def("createUniformInterface",&Interfacer<F>::createUniformInterface)
+  
     .def("insertPatternfromFile",&Interfacer<F>::insertPatternfromFile)
     .def("createNormalDistributedInterface",&Interfacer<F>::createNormalDistributedInterface)
 #ifdef CRACKLET_USE_LIBSURFER
@@ -32,11 +72,17 @@ void register_interfacer(py::module& mod) {
     .def("createThroughPolarAsperity",&Interfacer<F>::createThroughPolarAsperity)
     .def("createThroughMultiPolAsperity",&Interfacer<F>::createThroughMultiPolAsperity)
     .def("createThroughCenteredCrack",&Interfacer<F>::createThroughCenteredCrack)
-  .def("createThroughLeftSidedCrack",&Interfacer<F>::createThroughLeftSidedCrack)
-  .def("createRightPropagatingCrackRoundAsp",&Interfacer<F>::createRightPropagatingCrackRoundAsp)
-    .def("createIncohIntfc",&Interfacer<F>::createIncohIntfc);
-}
+    .def("createThroughLeftSidedCrack",&Interfacer<F>::createThroughLeftSidedCrack)
+    .def("createRightPropagatingCrackRoundAsp",&Interfacer<F>::createRightPropagatingCrackRoundAsp);
+    //.def("createIncohIntfc",&Interfacer<F>::createIncohIntfc);
+  }
+    
+  void register_interfacer(py::module& mod) {
+    wrap_interfacer_cohesive<_linear_coupled_cohesive>(mod);
+    wrap_interfacer_RANDS<_rate_and_state>(mod);
+    wrap_interfacer_RANDS<_regularized_rate_and_state>(mod);
+    wrap_interfacer_RANDS<_weakening_rate_and_state>(mod);
 
-  //void register_interfacer(py::module& mod) {
+}
   
 } // namespace cRacklet
--- python/py_interfacer.hh
+++ python/py_interfacer.hh
@@ -1,6 +1,8 @@
 #ifndef __CRACKLET_PY_INTERFACER_HH__
 #define __CRACKLET_PY_INTERFACER_HH__
 
+#include <iostream>
+
 namespace pybind11{
   struct module;
 }
@@ -9,7 +11,8 @@ namespace cRacklet{
 
   void register_interfacer(pybind11::module & mod);
   void register_fracture_law_type(pybind11::module & mod);
-
+  //std::string FractureLawToString(FractureLawType F);
+  
 }
 
 #endif

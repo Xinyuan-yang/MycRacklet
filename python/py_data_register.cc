3bb256992be331680442265785a8904edf02610c
Thibault Roch <thibault.roch@epfl.ch>
2019-12-24 14:30:20 +0100

    adding readData

--- python/py_data_register.cc
+++ python/py_data_register.cc
@@ -57,6 +57,7 @@ void register_data_register(py::module& mod) {
   py::class_<DataRegister>(mod, "DataRegister")
     .def(py::init<>())
     .def_readwrite_static("restart_dir",&DataRegister::restart_dir)
+    .def("readData",&DataRegister::readData)
     .def("registerParameter",&DataRegister::registerParameter)
     .def("readInputFile",&DataRegister::readInputFile)
     .def("getParameter",&DataRegister::getParameter)

7e2aaf436d3b7da7a8f413e2f3e5040fbc4168f8
Thibault.Roch <thibault.roch@epfl.ch>
2020-06-30 17:00:08 +0200

    Fix to include FFTW and GSL headers if they are not installed in default /include

--- cmake/Modules/FindFFTW.cmake
+++ cmake/Modules/FindFFTW.cmake
@@ -1,10 +1,10 @@
-set(FFTW_VERSION "3" CACHE INTEGER "Version of FFTW required")
+set(FFTW_VERSION 3 CACHE STRING "Version of FFTW required")
 
 if (FFTW_FIND_VERSION)
   set(FFTW_VERSION ${FFTW_FIND_VERSION} CACHE INTEGER "Version of FFTW required")
 endif()
 
-if (FFTW_VERSION EQUAL "2")
+if (FFTW_VERSION EQUAL 2)
   find_library(FFTW_LIBRARIES fftw
     PATHS ${FFTW_DIR}
     PATH_SUFFIXES fftw/.libs/ lib

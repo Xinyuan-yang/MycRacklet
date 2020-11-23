85baf7ff6c98ba7560f8ae8133636769335657ff
Thibault Roch <thibault.roch@epfl.ch>
2020-11-23 08:48:00 +0100

    Wrap interfacer for viscoealstic and example in python

--- python/py_interfacer.cc
+++ python/py_interfacer.cc
@@ -19,6 +19,10 @@ std::string FractureLawToString(FractureLawType F) {
     case _linear_coupled_cohesive:
       name = "LinearCoupledCohesive"; 
       break;
+
+    case _viscoelastic_coupled_cohesive:
+      name = "CohesiveViscoelastic"; 
+      break;
       
     case _rate_and_state:
       name = "RateAndState"; 
@@ -40,50 +44,63 @@ std::string FractureLawToString(FractureLawType F) {
 void register_fracture_law_type(py::module&mod){
   py::enum_<FractureLawType>(mod,"FractureLawType")
     .value("_linear_coupled_cohesive",FractureLawType::_linear_coupled_cohesive)
+    .value("_viscoelastic_coupled_cohesive",FractureLawType::_viscoelastic_coupled_cohesive)
     .value("_rate_and_state",FractureLawType::_rate_and_state)
     .value("_regularized_rate_and_state",FractureLawType::_regularized_rate_and_state)
     .value("_weakening_rate_and_state",FractureLawType::_weakening_rate_and_state);
   }
   
-template<FractureLawType F>
-void wrap_interfacer_RANDS(py::module& mod) {
-  std::string class_name = FractureLawToString(F);
-  py::class_<Interfacer<F>,DataRegister>(mod,class_name.c_str())
-    .def(py::init<SpectralModel&>())
-    .def("createUniformInterface",&Interfacer<F>::createUniformInterface);
-}
-
+  template<FractureLawType F>
+  void wrap_interfacer_RANDS(py::module& mod) {
+    std::string class_name = FractureLawToString(F);
+    py::class_<Interfacer<F>,DataRegister>(mod,class_name.c_str())
+      .def(py::init<SpectralModel&>())
+      .def("createUniformInterface",&Interfacer<F>::createUniformInterface);
+  }
   
-template<FractureLawType F>
-void wrap_interfacer_cohesive(py::module& mod) {
-  std::string class_name = FractureLawToString(F);
-  py::class_<Interfacer<F>,DataRegister>(mod,class_name.c_str())
-    .def(py::init<SpectralModel&>())
-    .def("createUniformInterface",&Interfacer<F>::createUniformInterface)
   
-    .def("insertPatternfromFile",py::overload_cast<std::string, UInt>(&Interfacer<F>::insertPatternfromFile))
-    .def("insertPatternfromFile",py::overload_cast<std::string, std::string>(&Interfacer<F>::insertPatternfromFile)) // Create an interface based on two files: one for the strength, the other for the opening
-    .def("createNormalDistributedInterface",&Interfacer<F>::createNormalDistributedInterface)
+  template<FractureLawType F>
+  void wrap_interfacer_cohesive(py::module& mod) {
+    std::string class_name = FractureLawToString(F);
+    py::class_<Interfacer<F>,DataRegister>(mod,class_name.c_str())
+      .def(py::init<SpectralModel&>())
+      .def("createUniformInterface",&Interfacer<F>::createUniformInterface)
+      
+      .def("insertPatternfromFile",py::overload_cast<std::string, UInt>(&Interfacer<F>::insertPatternfromFile))
+      .def("insertPatternfromFile",py::overload_cast<std::string, std::string>(&Interfacer<F>::insertPatternfromFile)) // Create an interface based on two files: one for the strength, the other for the opening
+      .def("createNormalDistributedInterface",&Interfacer<F>::createNormalDistributedInterface)
 #ifdef CRACKLET_USE_LIBSURFER
-    .def("createBrownianHeterogInterface",&Interfacer<F>::createBrownianHeterogInterface)
+      .def("createBrownianHeterogInterface",&Interfacer<F>::createBrownianHeterogInterface)
 #endif
-    .def("createThroughArea",&Interfacer<F>::createThroughArea)
-    .def("createThroughCrack",&Interfacer<F>::createThroughCrack)
-    .def("createThroughWall",&Interfacer<F>::createThroughWall)
-    .def("createThroughPolarAsperity",&Interfacer<F>::createThroughPolarAsperity)
-    .def("createThroughMultiPolAsperity",&Interfacer<F>::createThroughMultiPolAsperity)
-    .def("createThroughCenteredCrack",&Interfacer<F>::createThroughCenteredCrack)
-    .def("createThroughLeftSidedCrack",&Interfacer<F>::createThroughLeftSidedCrack)
-    .def("createRightPropagatingCrackRoundAsp",&Interfacer<F>::createRightPropagatingCrackRoundAsp);
+      .def("createThroughArea",&Interfacer<F>::createThroughArea)
+      .def("createThroughCrack",&Interfacer<F>::createThroughCrack)
+      .def("createThroughWall",&Interfacer<F>::createThroughWall)
+      .def("createThroughPolarAsperity",&Interfacer<F>::createThroughPolarAsperity)
+      .def("createThroughMultiPolAsperity",&Interfacer<F>::createThroughMultiPolAsperity)
+      .def("createThroughCenteredCrack",&Interfacer<F>::createThroughCenteredCrack)
+      .def("createThroughLeftSidedCrack",&Interfacer<F>::createThroughLeftSidedCrack)
+      .def("createRightPropagatingCrackRoundAsp",&Interfacer<F>::createRightPropagatingCrackRoundAsp);
     //.def("createIncohIntfc",&Interfacer<F>::createIncohIntfc);
   }
-    
+  
+  template<FractureLawType F>
+  void wrap_interfacer_viscoelastic(py::module& mod) {
+    std::string class_name = FractureLawToString(F);
+    py::class_<Interfacer<F>,DataRegister>(mod,class_name.c_str())
+      .def(py::init<SpectralModel&>())
+      .def("createUniformInterface",&Interfacer<F>::createUniformInterface)
+      .def("createThroughArea",&Interfacer<F>::createThroughArea)
+      .def("createThroughCrack",&Interfacer<F>::createThroughCrack)
+      .def("createThroughWall",&Interfacer<F>::createThroughWall);
+       }
+  
   void register_interfacer(py::module& mod) {
     wrap_interfacer_cohesive<_linear_coupled_cohesive>(mod);
+    wrap_interfacer_viscoelastic<_viscoelastic_coupled_cohesive>(mod);
     wrap_interfacer_RANDS<_rate_and_state>(mod);
     wrap_interfacer_RANDS<_regularized_rate_and_state>(mod);
     wrap_interfacer_RANDS<_weakening_rate_and_state>(mod);
-
+    
 }
   
 } // namespace cRacklet

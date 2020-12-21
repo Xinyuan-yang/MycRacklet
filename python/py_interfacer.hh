568a3bc51a8a1a320374e1ebb0a39a1b73fa8908
Thibault Roch <thibault.roch@epfl.ch>
2020-12-21 19:32:53 +0100

    update pybind files...

--- python/py_interfacer.hh
+++ python/py_interfacer.hh
@@ -1,11 +1,9 @@
+#include <pybind11/pybind11.h>
+
 #ifndef __CRACKLET_PY_INTERFACER_HH__
 #define __CRACKLET_PY_INTERFACER_HH__
 
 #include <iostream>
-
-namespace pybind11{
-  struct module;
-}
 
 namespace cRacklet{
 
--- python/py_rate_and_state_law.hh
+++ python/py_rate_and_state_law.hh
@@ -1,9 +1,7 @@
+#include <pybind11/pybind11.h>
+
 #ifndef __CRACKLET_PY_RATE_AND_STATE_LAW_HH__
 #define __CRACKLET_PY_RATE_AND_STATE_LAW_HH__
-
-namespace pybind11{
-  struct module;
-}
 
 namespace cRacklet{
 

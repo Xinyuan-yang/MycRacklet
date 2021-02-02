de89dce99df4a027a71c002c18e1a7e7ae3aa581
Thibault Roch <thibault.roch@epfl.ch>
2021-02-02 13:25:40 +0100

    Default arguments for sim driver

--- python/py_simulation_driver.cc
+++ python/py_simulation_driver.cc
@@ -23,7 +23,7 @@ namespace cRacklet {
       .def(py::init<SpectralModel&, Real, Real>())
       .def("initConstantLoading",&SimulationDriver::initConstantLoading)
       .def("initLoadingFromFile",&SimulationDriver::initLoadingFromFile)
-      .def("initConstantSpeed",&SimulationDriver::initConstantSpeed)
+      .def("initConstantSpeed",&SimulationDriver::initConstantSpeed,py::arg("initial_loading"),py::arg("psi"),py::arg("phi"),py::arg("average_max_stress"),py::arg("spont_crack_length")=0.0,py::arg("load_control")=LoadControlType::_time_control,py::arg("load_upper_bound")=0.9,py::arg("griffith_length")=0.)
       .def("solveStep",&SimulationDriver::solveStep)
       .def("writeLoading",&SimulationDriver::writeLoading)
       .def("launchCrack",&SimulationDriver::launchCrack,py::arg("crack_start"),py::arg("launched_size"),py::arg("v_init"),py::arg("one_side_propagation")=true);

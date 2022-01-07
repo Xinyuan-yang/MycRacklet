784fed956907c26f7f5e59c80bb13add3b89a9b2
Thibault Roch <thibault.roch@epfl.ch>
2022-01-07 17:39:59 +0100

    fix overload cast call

--- python/py_spectral_model.cc
+++ python/py_spectral_model.cc
@@ -100,7 +100,8 @@ void register_spectral_model(py::module& mod) {
     .def("updateLoads",[](SpectralModel & self) {self.updateLoads();
 		       },
 	 "Update loading case")
-    .def("updateLoads",py::overload_cast<Real *>(&SpectralModel::updateLoads),
+    .def("updateLoads",[](SpectralModel & self, Real * loading_per_dim){self.updateLoads(loading_per_dim);
+		       },
 	 "Update point-wise loading conidtions using an uniform constant value per dimension")
     .def("initInterfaceFields",&SpectralModel::initInterfaceFields,
 	 "Set the initial values of interface fields (strength,traction,velocities) using the interface conditions given in the associated InterfaceLaw")
